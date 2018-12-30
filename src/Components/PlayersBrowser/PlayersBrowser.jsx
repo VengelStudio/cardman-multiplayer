f6c76c4c9bc9628edc2065dbc7ae98c99ad88030
Łukasz Blachnicki <superkwek0@gmail.com>
2018-12-30 19:52:42 +0100

    Netlify settings updated.

--- src/Components/PlayersBrowser/PlayersBrowser.jsx
+++ src/Components/PlayersBrowser/PlayersBrowser.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "./PlayersBrowser.css";
-import Scrollbar from "react-scrollbars-custom";
+import React from 'react';
+import './PlayersBrowser.css';
+import Scrollbar from 'react-scrollbars-custom';
 
 const BrowserEntry = props => {
-  let classes = "browser-entry width-full ";
+  let classes = 'browser-entry width-full ';
   if (props.lightColor === true) {
-    classes += "browser-entry-lightbg";
+    classes += 'browser-entry-lightbg';
   } else if (props.lightColor === false) {
-    classes += "browser-entry-darkbg";
+    classes += 'browser-entry-darkbg';
   }
 
   const handleGameStart = props => {
@@ -16,18 +16,15 @@ const BrowserEntry = props => {
 
   return (
     <div className={classes}>
-      <span className="player-info">
-        <img
-          className="country"
-          src="https://www.countryflags.io/pl/flat/64.png"
-        />
-        <span className="nickname">{props.nickname}</span>
+      <span className='player-info'>
+        <img className='country' src='https://www.countryflags.io/pl/flat/64.png' />
+        <span className='nickname'>{props.nickname}</span>
       </span>
       <button
         onClick={e => {
           handleGameStart(props);
         }}
-        className="play border-neon border-neon-lime"
+        className='play border-neon border-neon-lime'
       >
         Play
       </button>
@@ -39,33 +36,7 @@ class PlayersBrowser extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: [
-        { nickname: "Dezanek" },
-        { nickname: "Szymek" },
-        { nickname: "Kobus" },
-        { nickname: "Pawlacz" },
-        { nickname: "Twoja stara" },
-        { nickname: "Dezanek" },
-        { nickname: "Szymek" },
-        { nickname: "Kobus" },
-        { nickname: "Pawlacz" },
-        { nickname: "Twoja stara" },
-        { nickname: "Dezanek" },
-        { nickname: "Szymek" },
-        { nickname: "Kobus" },
-        { nickname: "Pawlacz" },
-        { nickname: "Twoja stara" },
-        { nickname: "Dezanek" },
-        { nickname: "Szymek" },
-        { nickname: "Kobus" },
-        { nickname: "Pawlacz" },
-        { nickname: "Twoja stara" },
-        { nickname: "Dezanek" },
-        { nickname: "Szymek" },
-        { nickname: "Kobus" },
-        { nickname: "Pawlacz" },
-        { nickname: "Twoja stara" }
-      ]
+      players: [{ nickname: 'Dezanek' }]
     };
     this.entries = this.getEntries(this.state.players);
   }
@@ -76,11 +47,21 @@ class PlayersBrowser extends React.Component {
       let nickname = players[index].nickname;
       if (index % 2 === 0) {
         result.push(
-          <BrowserEntry gameStartHandler={this.props.gameStartHandler} nickname={nickname} key={index} lightColor={true} />
+          <BrowserEntry
+            gameStartHandler={this.props.gameStartHandler}
+            nickname={nickname}
+            key={index}
+            lightColor={true}
+          />
         );
       } else {
         result.push(
-          <BrowserEntry gameStartHandler={this.props.gameStartHandler} nickname={nickname} key={index} lightColor={false} />
+          <BrowserEntry
+            gameStartHandler={this.props.gameStartHandler}
+            nickname={nickname}
+            key={index}
+            lightColor={false}
+          />
         );
       }
     }
@@ -89,13 +70,13 @@ class PlayersBrowser extends React.Component {
 
   render() {
     return (
-      <div className="players-browser container content-vcenter border-neon border-neon-orange">
-        <div className="players-browser-title bg-lightgrey width-full text-xlg text-center">
+      <div className='players-browser container content-vcenter border-neon border-neon-orange'>
+        <div className='players-browser-title bg-lightgrey width-full text-xlg text-center'>
           <p>
             You are logged in as <b>{this.props.nickname}</b>.
           </p>
         </div>
-        <Scrollbar style={{ width: "100%", height: "100%" }}>
+        <Scrollbar style={{ width: '100%', height: '100%' }}>
           {this.entries.map(entry => {
             return entry;
           })}
