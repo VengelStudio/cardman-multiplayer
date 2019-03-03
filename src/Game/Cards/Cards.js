a93c371d4328273476a5a82707262310df5d0788
Bartek <bartoszkepka1999@gmail.com>
2019-03-03 14:48:29 +0100

    Uncommented other cards

--- src/Game/Cards/Cards.js
+++ src/Game/Cards/Cards.js
@@ -11,111 +11,111 @@ const Cards = {
             return true
         }
     },
-    // RANDOM_CORRECT_LETTER_CARD: {
-    //     id: 'RANDOM_CORRECT_LETTER_CARD',
-    //     title: 'Random correct letter',
-    //     description: 'Chooses a random correct letter.',
-    //     use: ({ currentGame, socket, move }) => {
-    //         let randomCorrectLetter = () => {
-    //             let { word } = currentGame.word
-    //             let { guessed } = currentGame
-    //             let guessedArr = guessed.map(g => g.key.toUpperCase())
-    //             word = word.toUpperCase()
-    //             let arr = Array.from(word).filter(char => {
-    //                 return !guessedArr.includes(char)
-    //             })
-    //             let randomIndex = Math.floor(Math.random() * arr.length)
-    //             return arr[randomIndex].toUpperCase()
-    //         }
+    RANDOM_CORRECT_LETTER_CARD: {
+        id: 'RANDOM_CORRECT_LETTER_CARD',
+        title: 'Random correct letter',
+        description: 'Chooses a random correct letter.',
+        use: ({ currentGame, socket, move }) => {
+            let randomCorrectLetter = () => {
+                let { word } = currentGame.word
+                let { guessed } = currentGame
+                let guessedArr = guessed.map(g => g.key.toUpperCase())
+                word = word.toUpperCase()
+                let arr = Array.from(word).filter(char => {
+                    return !guessedArr.includes(char)
+                })
+                let randomIndex = Math.floor(Math.random() * arr.length)
+                return arr[randomIndex].toUpperCase()
+            }
 
-    //         let key = randomCorrectLetter()
-    //         currentGame.guessed.push({
-    //             key,
-    //             playerSocketId: move.playerSocketId
-    //         })
-    //         let playerSocket =
-    //             currentGame.playerSockets[currentGame.nextPlayerIndex].socketId
-    //         let enemySocket =
-    //             currentGame.playerSockets[1 - currentGame.nextPlayerIndex]
-    //                 .socketId
-    //         currentGame.keys.push({
-    //             key,
-    //             playerSocketId: playerSocket
-    //         })
-    //         currentGame.keys.push({
-    //             key,
-    //             playerSocketId: enemySocket
-    //         })
-    //         return currentGame
-    //     },
-    //     doesMeetConditions: game => {
-    //         return true
-    //     }
-    // },
-    // ADDITIONAL_TURN_CARD: {
-    //     id: 'ADDITIONAL_TURN_CARD',
-    //     title: 'Additional letter',
-    //     description: 'You can choose two letters in a turn.',
-    //     use: ({ currentGame, socket, move }) => {
-    //         console.log('ADDITIONAL_TURN_CARD card used')
-    //         currentGame.nextPlayerIndex = 1 - currentGame.nextPlayerIndex
-    //         return currentGame
-    //     },
-    //     doesMeetConditions: game => {
-    //         return true
-    //     }
-    // },
-    // REMOVE_ONE_UNFITTING_CARD: {
-    //     id: 'REMOVE_ONE_UNFITTING_CARD',
-    //     title: 'Remove one unfitting letter.',
-    //     description: 'Removes one unfitting letter from the current word.',
-    //     use: ({ currentGame, socket, move }) => {
-    //         let { word } = currentGame.word
-    //         let wordKeys = Array.from(word.toUpperCase())
-    //         let myKeys = []
-    //         currentGame.keys.forEach(key => {
-    //             if (key.playerSocketId === socket.user.socketId) {
-    //                 myKeys.push(key.key)
-    //             }
-    //         })
-    //         let candidates = []
-    //         for (let i = 65; i <= 90; i++) {
-    //             let letter = String.fromCharCode(i).toUpperCase()
-    //             if (!wordKeys.includes(letter) && !myKeys.includes(letter)) {
-    //                 candidates.push(letter)
-    //             }
-    //         }
-    //         currentGame.keys.push({
-    //             key: candidates[Math.floor(Math.random() * candidates.length)],
-    //             playerSocketId: socket.user.socketId
-    //         })
-    //         return currentGame
-    //     },
-    //     doesMeetConditions: game => {
-    //         return true
-    //     }
-    // },
-    // REMOVE_TWO_UNFITTING_CARD: {
-    //     id: 'REMOVE_TWO_UNFITTING_CARD',
-    //     title: 'Remove two unfitting letters.',
-    //     description: 'Removes two unfitting letters from the current word.',
-    //     use: ({ currentGame, socket, move }) => {
-    //         currentGame = Cards.REMOVE_ONE_UNFITTING_CARD.use({
-    //             currentGame,
-    //             socket,
-    //             move
-    //         })
-    //         currentGame = Cards.REMOVE_ONE_UNFITTING_CARD.use({
-    //             currentGame,
-    //             socket,
-    //             move
-    //         })
-    //         return currentGame
-    //     },
-    //     doesMeetConditions: game => {
-    //         return true
-    //     }
-    // },
+            let key = randomCorrectLetter()
+            currentGame.guessed.push({
+                key,
+                playerSocketId: move.playerSocketId
+            })
+            let playerSocket =
+                currentGame.playerSockets[currentGame.nextPlayerIndex].socketId
+            let enemySocket =
+                currentGame.playerSockets[1 - currentGame.nextPlayerIndex]
+                    .socketId
+            currentGame.keys.push({
+                key,
+                playerSocketId: playerSocket
+            })
+            currentGame.keys.push({
+                key,
+                playerSocketId: enemySocket
+            })
+            return currentGame
+        },
+        doesMeetConditions: game => {
+            return true
+        }
+    },
+    ADDITIONAL_TURN_CARD: {
+        id: 'ADDITIONAL_TURN_CARD',
+        title: 'Additional letter',
+        description: 'You can choose two letters in a turn.',
+        use: ({ currentGame, socket, move }) => {
+            console.log('ADDITIONAL_TURN_CARD card used')
+            currentGame.nextPlayerIndex = 1 - currentGame.nextPlayerIndex
+            return currentGame
+        },
+        doesMeetConditions: game => {
+            return true
+        }
+    },
+    REMOVE_ONE_UNFITTING_CARD: {
+        id: 'REMOVE_ONE_UNFITTING_CARD',
+        title: 'Remove one unfitting letter.',
+        description: 'Removes one unfitting letter from the current word.',
+        use: ({ currentGame, socket, move }) => {
+            let { word } = currentGame.word
+            let wordKeys = Array.from(word.toUpperCase())
+            let myKeys = []
+            currentGame.keys.forEach(key => {
+                if (key.playerSocketId === socket.user.socketId) {
+                    myKeys.push(key.key)
+                }
+            })
+            let candidates = []
+            for (let i = 65; i <= 90; i++) {
+                let letter = String.fromCharCode(i).toUpperCase()
+                if (!wordKeys.includes(letter) && !myKeys.includes(letter)) {
+                    candidates.push(letter)
+                }
+            }
+            currentGame.keys.push({
+                key: candidates[Math.floor(Math.random() * candidates.length)],
+                playerSocketId: socket.user.socketId
+            })
+            return currentGame
+        },
+        doesMeetConditions: game => {
+            return true
+        }
+    },
+    REMOVE_TWO_UNFITTING_CARD: {
+        id: 'REMOVE_TWO_UNFITTING_CARD',
+        title: 'Remove two unfitting letters.',
+        description: 'Removes two unfitting letters from the current word.',
+        use: ({ currentGame, socket, move }) => {
+            currentGame = Cards.REMOVE_ONE_UNFITTING_CARD.use({
+                currentGame,
+                socket,
+                move
+            })
+            currentGame = Cards.REMOVE_ONE_UNFITTING_CARD.use({
+                currentGame,
+                socket,
+                move
+            })
+            return currentGame
+        },
+        doesMeetConditions: game => {
+            return true
+        }
+    },
     BLOCK_CARD: {
         id: 'BLOCK_CARD',
         title: 'Block enemy cards for 2 turns.',
@@ -136,8 +136,16 @@ const Cards = {
     //     id: 'IMMUNE_TO_BLOCK_CARD',
     //     title: 'Immune to block.',
     //     description: 'Immune to block for 2 turns.',
-    //     use: () => {
-    //         console.log('IMMUNE_TO_BLOCK_CARD card used')
+    //     use: ({ currentGame, socket, move }) => {
+    //         let mySocket = move.playerSocketId
+    //         let enemySocket = currentGame.playerSockets.filter(e => {
+    //             return e.socketId != move.playerSocketId
+    //         })[0].socketId
+    //         currentGame.blockCounters[enemySocket] = -2
+    //         return currentGame
+    //     },
+    //     doesMeetConditions: game => {
+    //         return true
     //     }
     // },
     // LOOK_UP_CARD: {
