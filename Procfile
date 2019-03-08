b29ffbf4626f57ad5d0143dd3e911d4d388dee47
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-03-08 23:56:56 +0100

    Set up server, added express and Heroku procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./src/Server/index.js
--- src/Server/index.js
+++ src/Server/index.js
@@ -1,12 +1,25 @@
-var app = require('http').createServer()
-var io = (module.exports.io = require('socket.io')(app, { pingInterval: 2000 }))
-
-const PORT = process.env.PORT || 3231
-
+const path = require('path')
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = (module.exports.io = require('socket.io')(server, {
+    pingInterval: 2000
+}))
+const port = process.env.PORT || 3231
 const SocketManager = require('./SocketManager')
+
+app.use(express.static(path.join(__dirname, '../../build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build/index.html'), err => {
+        if (err) {
+            res.status(500).send(err)
+        }
+    })
+})
 
 io.on('connection', SocketManager)
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}.`)
+server.listen(port, () => {
+    console.log(`Server started on port ${port}.`)
 })
