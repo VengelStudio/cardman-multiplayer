c6a0e025d672352f0d63fb595372a5d044a6d5e3
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-03-09 14:27:27 +0100

    Added try/catch statements in server code

--- src/Server/SocketManager.js
+++ src/Server/SocketManager.js
@@ -59,12 +59,16 @@ module.exports = function(socket) {
     })
 
     socket.on(PLAYER_CONNECTED, player => {
-        player.socketId = socket.id
-        connectedPlayers = addPlayer(player, connectedPlayers)
-        socket.user = player
+        try {
+            player.socketId = socket.id
+            connectedPlayers = addPlayer(player, connectedPlayers)
+            socket.user = player
 
-        socket.emit(PLAYER_CONNECTED, { connectedPlayers })
-        io.emit(REFRESH_PLAYERS, { connectedPlayers })
+            socket.emit(PLAYER_CONNECTED, { connectedPlayers })
+            io.emit(REFRESH_PLAYERS, { connectedPlayers })
+        } catch (e) {
+            console.log(e)
+        }
     })
 
     socket.on('disconnect', () => {
@@ -131,59 +135,67 @@ module.exports = function(socket) {
     })
 
     socket.on(INVITATION, ({ id = null, socketId = null }) => {
-        let nickname = socket.user.nickname
-        let fromSocketId = socket.user.socketId
-        if (socket.user.id === id) {
-            console.log(`[ERROR] ${nickname} tried to invite himself.`)
-        } else {
-            console.log(`[INVITATION] from ${socket.user.id} to ${id}.`)
-            socket.to(socketId).emit(INVITATION, {
-                socketId: fromSocketId,
-                nickname
-            })
+        try {
+            let nickname = socket.user.nickname
+            let fromSocketId = socket.user.socketId
+            if (socket.user.id === id) {
+                console.log(`[ERROR] ${nickname} tried to invite himself.`)
+            } else {
+                console.log(`[INVITATION] from ${socket.user.id} to ${id}.`)
+                socket.to(socketId).emit(INVITATION, {
+                    socketId: fromSocketId,
+                    nickname
+                })
+            }
+        } catch (e) {
+            console.log(e)
         }
     })
 
     socket.on(INVITATION_ACCEPTED, ({ fromSocketId, to }) => {
         console.log(`[INVITATION] from: ${fromSocketId}, to: ${to.socketId}.`)
 
-        let playerSockets = [
-            io.sockets.connected[fromSocketId].user,
-            io.sockets.connected[to.socketId].user
-        ]
+        try {
+            let playerSockets = [
+                io.sockets.connected[fromSocketId].user,
+                io.sockets.connected[to.socketId].user
+            ]
 
-        let game = createGame({
-            word: getRandomWord(words),
-            playerSockets,
-            nextPlayerIndex: Math.round(Math.random())
-        })
+            let game = createGame({
+                word: getRandomWord(words),
+                playerSockets,
+                nextPlayerIndex: Math.round(Math.random())
+            })
 
-        game.displayWord = displayWord(game)
+            game.displayWord = displayWord(game)
 
-        for (let i = 0; i <= 1; i++) {
-            game.score[playerSockets[i].socketId] = 0
-            game.blockCounters[playerSockets[i].socketId] = 0
-            game.cards[playerSockets[i].socketId] = generateCards(3)
+            for (let i = 0; i <= 1; i++) {
+                game.score[playerSockets[i].socketId] = 0
+                game.blockCounters[playerSockets[i].socketId] = 0
+                game.cards[playerSockets[i].socketId] = generateCards(3)
+            }
+
+            games = addGame(game, games)
+            io.sockets.connected[fromSocketId].join(game.id)
+            io.sockets.connected[to.socketId].join(game.id)
+
+            //* players are in game
+            connectedPlayers = setPlayersInGameStatus(
+                connectedPlayers,
+                playerSockets,
+                true,
+                game
+            )
+
+            io.emit(REFRESH_PLAYERS, { connectedPlayers })
+            io.in(game.id).emit(GAME_CREATED, { gameId: game.id })
+
+            console.log(
+                `[GAME] ${fromSocketId} vs ${to.socketId}, gameID: ${game.id}.`
+            )
+        } catch (e) {
+            console.log(e)
         }
-
-        games = addGame(game, games)
-        io.sockets.connected[fromSocketId].join(game.id)
-        io.sockets.connected[to.socketId].join(game.id)
-
-        //* players are in game
-        connectedPlayers = setPlayersInGameStatus(
-            connectedPlayers,
-            playerSockets,
-            true,
-            game
-        )
-
-        io.emit(REFRESH_PLAYERS, { connectedPlayers })
-        io.in(game.id).emit(GAME_CREATED, { gameId: game.id })
-
-        console.log(
-            `[GAME] ${fromSocketId} vs ${to.socketId}, gameID: ${game.id}.`
-        )
     })
 
     socket.on(WALKTHROUGH_READY, ({ gameId }) => {
@@ -194,87 +206,97 @@ module.exports = function(socket) {
     })
 
     socket.on(GAME_MOVE, ({ game, moves }) => {
-        let currentGame = games[game.id]
-        let player = game.playerSockets[game.nextPlayerIndex]
-        let enemy = game.playerSockets[1 - game.nextPlayerIndex]
+        try {
+            let currentGame = games[game.id]
+            let player = game.playerSockets[game.nextPlayerIndex]
+            let enemy = game.playerSockets[1 - game.nextPlayerIndex]
 
-        let blockCounter = currentGame.blockCounters[player.socketId]
-        if (player.id === socket.user.id) {
-            moves = moves.sort((a, b) => {
-                if (a.type === 'key') return -1
-                else return 1
-            })
-            moves.forEach(move => {
-                if (move.type === 'key') {
-                    currentGame.guessed.push({
-                        key: move.key,
-                        playerSocketId: move.playerSocketId
-                    })
-                    currentGame.keys.push({
-                        key: move.key,
-                        playerSocketId: move.playerSocketId
-                    })
-                    currentGame.keys.push({
-                        key: move.key,
-                        playerSocketId: enemy.socketId
-                    })
-                } else if (move.type === 'card' && blockCounter === 0) {
-                    let cardName = move.card
-                    let card = getCard(cardName)
-                    if (move.discarded === false) {
-                        currentGame = card.use({ currentGame, socket, move })
-                    } else {
-                        console.log('discarded')
+            let blockCounter = currentGame.blockCounters[player.socketId]
+            if (player.id === socket.user.id) {
+                moves = moves.sort((a, b) => {
+                    if (a.type === 'key') return -1
+                    else return 1
+                })
+                moves.forEach(move => {
+                    if (move.type === 'key') {
+                        currentGame.guessed.push({
+                            key: move.key,
+                            playerSocketId: move.playerSocketId
+                        })
+                        currentGame.keys.push({
+                            key: move.key,
+                            playerSocketId: move.playerSocketId
+                        })
+                        currentGame.keys.push({
+                            key: move.key,
+                            playerSocketId: enemy.socketId
+                        })
+                    } else if (move.type === 'card' && blockCounter === 0) {
+                        let cardName = move.card
+                        let card = getCard(cardName)
+                        if (move.discarded === false) {
+                            currentGame = card.use({
+                                currentGame,
+                                socket,
+                                move
+                            })
+                        } else {
+                            console.log('discarded')
+                        }
+                        currentGame.cards = removeUsedCard(
+                            currentGame,
+                            card,
+                            move.playerSocketId
+                        )
                     }
-                    currentGame.cards = removeUsedCard(
-                        currentGame,
-                        card,
-                        move.playerSocketId
-                    )
+                })
+                if (blockCounter > 0)
+                    currentGame.blockCounters[player.socketId] =
+                        blockCounter - 1
+                if (blockCounter < 0)
+                    currentGame.blockCounters[player.socketId] =
+                        blockCounter + 1
+
+                currentGame.displayWord = displayWord(currentGame)
+
+                const debugMode = false
+
+                let result = checkWin(currentGame, socket)
+                if (debugMode) result = Result.TURN_WIN
+                currentGame.nextPlayerIndex = 1 - currentGame.nextPlayerIndex
+
+                if (result !== Result.NOTHING) {
+                    let win = handleWin(currentGame, result)
+                    currentGame = win.game
+                    let winType = win.winObject.type
+                    if (winType === Result.GAME_WIN) {
+                        connectedPlayers = setPlayersInGameStatus(
+                            connectedPlayers,
+                            currentGame.playerSockets,
+                            false
+                        )
+                        games = removeGame(game, games)
+                    } else if (
+                        winType === Result.TURN_WIN ||
+                        winType === Result.TURN_TIE
+                    ) {
+                        currentGame.cards = resupplyCards(currentGame)
+                        currentGame.blockCounters[player.socketId] = 0
+                        currentGame.blockCounters[enemy.socketId] = 0
+                    }
+                    io.in(game.id).emit(WIN, win.winObject)
+                    if (winType === Result.GAME_WIN) {
+                        io.sockets.connected[player.socketId].leave(game.id)
+                        io.sockets.connected[enemy.socketId].leave(game.id)
+                    }
+                    io.emit(REFRESH_PLAYERS, { connectedPlayers })
+                    return
                 }
-            })
-            if (blockCounter > 0)
-                currentGame.blockCounters[player.socketId] = blockCounter - 1
-            if (blockCounter < 0)
-                currentGame.blockCounters[player.socketId] = blockCounter + 1
-
-            currentGame.displayWord = displayWord(currentGame)
-
-            const debugMode = false
-
-            let result = checkWin(currentGame, socket)
-            if (debugMode) result = Result.TURN_WIN
-            currentGame.nextPlayerIndex = 1 - currentGame.nextPlayerIndex
-
-            if (result !== Result.NOTHING) {
-                let win = handleWin(currentGame, result)
-                currentGame = win.game
-                let winType = win.winObject.type
-                if (winType === Result.GAME_WIN) {
-                    connectedPlayers = setPlayersInGameStatus(
-                        connectedPlayers,
-                        currentGame.playerSockets,
-                        false
-                    )
-                    games = removeGame(game, games)
-                } else if (
-                    winType === Result.TURN_WIN ||
-                    winType === Result.TURN_TIE
-                ) {
-                    currentGame.cards = resupplyCards(currentGame)
-                    currentGame.blockCounters[player.socketId] = 0
-                    currentGame.blockCounters[enemy.socketId] = 0
-                }
-                io.in(game.id).emit(WIN, win.winObject)
-                if (winType === Result.GAME_WIN) {
-                    io.sockets.connected[player.socketId].leave(game.id)
-                    io.sockets.connected[enemy.socketId].leave(game.id)
-                }
-                io.emit(REFRESH_PLAYERS, { connectedPlayers })
-                return
+                games[game.id] = currentGame
+                io.in(game.id).emit(GAME_MOVE, { game: games[game.id] })
             }
-            games[game.id] = currentGame
-            io.in(game.id).emit(GAME_MOVE, { game: games[game.id] })
+        } catch (e) {
+            console.log(e)
         }
     })
 }
