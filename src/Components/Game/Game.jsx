1f1678cad5548da020b26f5b2dceee59467a58cf
Kartm <lblachnicki@gmail.com>
2019-03-16 13:18:22 +0100

    Rewritten move mechanics

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -3,30 +3,43 @@ import { withRouter } from 'react-router-dom'
 
 import PropTypes from 'prop-types'
 import ReactAudioPlayer from 'react-audio-player'
+import discardSound from '../../Resources/Sounds/card_drop.mp3'
+import flipSound1 from '../../Resources/Sounds/card_flip.mp3'
+import flipSound2 from '../../Resources/Sounds/card_flip2.mp3'
+import flipSound3 from '../../Resources/Sounds/card_flip3.mp3'
+import buttonClick from '../../Resources/Sounds/button_click.mp3'
 
 import './Game.css'
-import Cards from './Cards'
+import Cards from './Cards/Cards'
 import Content from './Content'
 import GenericModal from '../Modals/GenericModal'
+import CardModal from '../Modals/CardModal'
 
+const { Cards: CardsData } = require('../../Game/Cards/Cards')
 const { setScore, isMove } = require('../../Shared/Functions')
 const { GAME_MOVE, WIN } = require('../../Shared/Events')
 const { Result } = require('../../Shared/Enums')
+
 class Game extends Component {
     state = {
         game: this.props.game,
         gameFromProps: true,
         allowMove: true,
-        myCards: null,
-        enemyCards: null,
-        cardTargetHighlight: false,
-        usedCardIndexes: { 0: false, 1: false, 2: false },
         soundSrc: '',
         isDiscardEnabled: false,
-        discardMoves: [],
         guessedWordModal: null,
         isTieModal: false,
-        gameEndWinnerModal: null
+        gameEndWinnerModal: null,
+        wordDefinition: null,
+        isMoveModal: false,
+        isPeekModal: false,
+        peekCardId: null,
+        peekDescription: null,
+
+        keyMove: null,
+        cardMoves: [],
+        myCards: null,
+        enemyCards: null
     }
 
     initializeSocket = () => {
@@ -91,13 +104,19 @@ class Game extends Component {
     }
 
     ctrlPressHandler(event, isDown) {
-        let { player } = this.props
+        let { player, isMove } = this.props
         let { game } = this.state
         if (player !== null && game !== null) {
             let mySocketId = player.socketId
             let myBlocked = game.blockCounters[mySocketId]
-            if (event.keyCode === 17 && myBlocked <= 0) {
-                this.setState({ isDiscardEnabled: isDown })
+            if (isMove && myBlocked <= 0) {
+                if (event.keyCode === 17) {
+                    this.setState({ isDiscardEnabled: isDown })
+                }
+            } else {
+                if (event.keyCode === 17) {
+                    this.setState({ isDiscardEnabled: false })
+                }
             }
         }
     }
@@ -136,64 +155,165 @@ class Game extends Component {
         )
     }
 
-    moveHandler = ({ moves = null }) => {
+    onMove = () => {
         if (this.state.allowMove === true) {
+            if (
+                this.state.keyMove !== null ||
+                this.state.cardMoves.length > 0
+            ) {
+                if (this.props.isMove) {
+                    let moves = []
+                    let { keyMove, cardMoves } = this.state
+                    if (keyMove !== null) moves.push(keyMove)
+                    if (cardMoves !== []) moves = [...moves, ...cardMoves]
+                    this.setState({
+                        keyMove: null,
+                        cardMoves: []
+                    })
+                    this.playSound(flipSound3)
+                    cardMoves.forEach(e => {
+                        if (e.discarded === false) {
+                            if (e.card === CardsData.DEFINITION_CARD.id) {
+                                let definitions = this.props.game.word
+                                    .definitions
+                                let randomIndex = Math.floor(
+                                    Math.random() * definitions.length
+                                )
+                                this.setState({
+                                    wordDefinition: definitions[randomIndex]
+                                })
+                            } else if (e.card === CardsData.LOOK_UP_CARD.id) {
+                                let enemySocket = this.props.game.playerSockets.filter(
+                                    x => {
+                                        return (
+                                            x.socketId !==
+                                            this.props.player.socketId
+                                        )
+                                    }
+                                )[0].socketId
+                                let enemyCards = this.props.game.cards[
+                                    enemySocket
+                                ]
+                                let randomIndexOfCard = Math.floor(
+                                    Math.random() * enemyCards.length
+                                )
+                                let randomEnemyCard =
+                                    enemyCards[randomIndexOfCard].id
+                                let { description } = CardsData[randomEnemyCard]
+                                this.setState({
+                                    isPeekModal: true,
+                                    peekCardId:
+                                        enemyCards[randomIndexOfCard].id,
+                                    peekDescription: description
+                                })
+                            }
+                        }
+                    })
+                }
+            } else {
+                this.setState({
+                    isMoveModal: true
+                })
+            }
+
             const { socket } = this.props
-            let allMoves = [...moves, ...this.state.discardMoves]
-            socket.emit(GAME_MOVE, { game: this.state.game, moves: allMoves })
+            let moves = [this.state.keyMove, ...this.state.cardMoves]
+            socket.emit(GAME_MOVE, { game: this.state.game, moves })
         }
     }
 
     onMoveTimeout = () => {
-        this.moveHandler({
-            moves: [
-                {
+        this.setState(
+            {
+                keyMove: {
                     type: 'key',
                     key: '',
                     playerSocketId: this.props.player.socketId
-                }
-            ]
-        })
-    }
-
-    setCardTargetHighlight = bool => {
-        this.setState({ cardTargetHighlight: bool })
-    }
-
-    updateUsedCardIndexes = newIndexes => {
-        this.setState({ usedCardIndexes: newIndexes })
+                },
+                cardMoves: []
+            },
+            () => {
+                this.onMove()
+            }
+        )
     }
 
     onUseAbort = index => {
-        let newIndexes = this.state.usedCardIndexes
-        newIndexes[index] = false
-        this.updateUsedCardIndexes(newIndexes)
+        let { cardMoves } = this.state
+        cardMoves = cardMoves.filter(card => {
+            return card.index !== index
+        })
+        this.playSound(flipSound2)
+        this.setState({ cardMoves })
     }
 
     playSound = src => {
         this.setState({ soundSrc: src })
     }
 
+    onKeyMove = move => {
+        if (this.props.isMove) {
+            this.playSound(buttonClick)
+            this.setState({ keyMove: move })
+        }
+    }
+
+    clearKeyMove = () => {
+        this.setState({
+            keyMove: null,
+            clickedIndex: null
+        })
+    }
+
+    onCardUse = data => {
+        if (this.props.isMove) {
+            let move = {
+                index: data.index,
+                type: 'card',
+                card: data.cardId,
+                playerSocketId: this.props.player.socketId,
+                discarded: false
+            }
+
+            let isDuplicate = this.state.cardMoves.some(
+                cardMove => cardMove.index === data.index
+            )
+            if (isDuplicate === false) {
+                setTimeout(() => {
+                    this.playSound(flipSound1)
+                }, 100)
+                this.setState({ cardMoves: [...this.state.cardMoves, move] })
+            }
+        }
+    }
+
     onDiscard = (index, cardId) => {
-        let move = {
+        let playerSocketId = this.props.player.socketId
+
+        let newMove = {
+            index,
             type: 'card',
             card: cardId,
-            playerSocketId: this.props.player.socketId,
+            playerSocketId,
             discarded: true
         }
-        let newGame = this.state.game
-        let mySocketId = this.props.player.socketId
-        newGame.cards[mySocketId] = newGame.cards[mySocketId].filter(
-            (val, i) => {
+        let { cardMoves } = this.state
+        cardMoves = cardMoves.filter(move => {
+            return move.index + cardMoves.length !== index
+        })
+        cardMoves.push(newMove)
+        this.setState({ cardMoves })
+
+        let game = this.state.game
+        game.cards[playerSocketId] = game.cards[playerSocketId].filter(
+            (e, i) => {
                 return i !== index
             }
         )
-        let newDiscardMoves = this.state.discardMoves
-        newDiscardMoves.push(move)
-        this.setState({
-            discardMoves: newDiscardMoves,
-            game: newGame
-        })
+        setTimeout(() => {
+            this.playSound(discardSound)
+        }, 100)
+        this.setState({ game })
     }
 
     render() {
@@ -209,6 +329,30 @@ class Game extends Component {
         }
         return (
             <div className='gameWrapper'>
+                {this.state.wordDefinition && (
+                    <GenericModal
+                        title='Word definition:'
+                        content={this.state.wordDefinition}
+                        onClose={() => this.setState({ wordDefinition: null })}
+                        volumeSettings={this.props.volumeSettings}
+                    />
+                )}
+                {this.state.isMoveModal && (
+                    <GenericModal
+                        title='Cannot continue!'
+                        content='You have to make a a move.'
+                        onClose={() => this.setState({ isMoveModal: false })}
+                        volumeSettings={this.props.volumeSettings}
+                    />
+                )}
+                {this.state.isPeekModal && (
+                    <CardModal
+                        cardId={this.state.peekCardId}
+                        description={this.state.peekDescription}
+                        onClose={() => this.setState({ isPeekModal: false })}
+                        volumeSettings={this.props.volumeSettings}
+                    />
+                )}
                 {this.state.guessedWordModal && (
                     <GenericModal
                         title='Guessed word:'
@@ -253,39 +397,28 @@ class Game extends Component {
                 />
                 <Cards
                     cards={cards.my}
+                    areMyCards={true}
+                    onCardUse={this.onCardUse}
+                    onDiscard={this.onDiscard}
                     onUseAbort={this.onUseAbort}
-                    usedCardIndexes={this.state.usedCardIndexes}
-                    areMine={true}
-                    move={this.props.isMove}
-                    title='Your cards:'
-                    setCardTargetHighlight={this.setCardTargetHighlight}
-                    playSound={this.playSound}
+                    isDiscardEnabled={this.state.isDiscardEnabled}
+                    isMove={this.props.isMove}
                     game={this.state.game}
                     player={this.props.player}
-                    isDiscardEnabled={this.state.isDiscardEnabled}
-                    onDiscard={this.onDiscard}
+                    cardMoves={this.state.cardMoves}
                 />
                 <Content
                     player={this.props.player}
-                    updateUsedCardIndexes={this.updateUsedCardIndexes}
-                    usedCardIndexes={this.state.usedCardIndexes}
-                    moveHandler={this.moveHandler}
                     onMoveTimeout={this.onMoveTimeout}
-                    move={this.props.isMove}
+                    isMove={this.props.isMove}
                     game={this.state.game}
-                    addPopup={this.props.addPopup}
-                    isCardTargetHighlight={this.state.cardTargetHighlight}
-                    playSound={this.playSound}
-                    volumeSettings={this.props.volumeSettings}
+                    keyMove={this.state.keyMove}
+                    onKeyMove={this.onKeyMove}
+                    onEndTurn={this.onMove}
                 />
                 <Cards
                     cards={cards.enemy}
-                    usedCardIndexes={{ 0: false, 1: false, 2: false }}
-                    areMine={false}
-                    move={!this.props.isMove}
-                    title='Enemy cards:'
-                    setCardTargetHighlight={this.setCardTargetHighlight}
-                    playSound={this.playSound}
+                    areMyCards={false}
                     game={this.state.game}
                     player={this.props.player}
                 />
--- src/Components/Game/Key.jsx
+++ src/Components/Game/Key.jsx
@@ -1,45 +1,26 @@
 import React, { Component } from 'react'
 
 class Key extends Component {
-    clickHandler = () => {
-        this.props.onClick()
-        if (this.props.isClicked === false) {
-            this.props.moveHandler({
-                move: {
-                    type: 'key',
-                    key: this.props.letter,
-                    playerSocketId: this.props.player.socketId
-                }
-            })
-        }
-    }
-
-    getStyle = () => {
-        let style = null
-        style = { backgroundColor: '#519C3F' }
-        if (this.props.isUsed) {
-            style = { backgroundColor: '#555', textDecoration: 'none' }
-        }
-        if (this.props.isClicked) {
+    render() {
+        let { isUsed, isSelected, letter, keyClickHandler } = this.props
+        let style = { backgroundColor: '#519C3F' }
+        if (isUsed) style = { backgroundColor: '#555', textDecoration: 'none' }
+        if (isSelected)
             style = {
                 ...style,
                 borderWidth: '0.3vh',
                 borderStyle: 'solid',
                 borderColor: 'orange'
             }
-        }
-
-        return style
-    }
-
-    render() {
         return (
             <button
-                style={this.getStyle()}
-                onClick={this.clickHandler}
+                style={style}
+                onClick={() => {
+                    keyClickHandler()
+                }}
                 className='key'
             >
-                {this.props.letter}
+                {letter}
             </button>
         )
     }
